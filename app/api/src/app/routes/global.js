0dcbbe8e31e7baa955386294ddaf0dea8b4a35c8
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2026-02-24 12:39:59 -0400

    :fire: remove: removendo arquivos e pastas que nao sao docs

--- app/api/src/app/routes/global.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import SessionController from "../controllers/SessionController";
-
-const routes = new Router()
-
-routes.post('/login', SessionController.store)
-
-export default routes
--- app/front/frontend/src/styles/Image.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Image = styled.img `
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    object-fit: cover;
-`
