07dce944ef20a6bfb574fed2dd4ada064017684a
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2026-02-24 13:07:02 -0400

    :truck: chore: renomeando importações para ES Modules

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,13 +1,13 @@
-module.exports = {
-    dialect: 'postgres', // qual banco de dados vai ser utilizado
-    host: '',
-    port: 5432,
-    username: 'postgres',
-    password: '',
-    database: '',
-    dialectOptions: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true
-    }
-}
+export default {
+	dialect: "postgres", // qual banco de dados vai ser utilizado
+	host: "",
+	port: 5432,
+	username: "postgres",
+	password: "",
+	database: "",
+	dialectOptions: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
